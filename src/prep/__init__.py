44799c3c301b36bc5da1f1bf6baf07d483d5f6ee
Moenupa <49304833+Moenupa@users.noreply.github.com>
2026-08-28 11:18:22 +0800

    [env] respect LOG_LEVEL env var

--- src/prep/__init__.py
+++ src/prep/__init__.py
@@ -1,16 +1,3 @@
-import logging
-
-from rich.logging import RichHandler
-
 from . import formatter  # noqa: F401 # trigger formatter registration
-
-logging.basicConfig(
-    format="%(message)s",
-    level=logging.INFO,
-    handlers=[RichHandler(show_time=False, show_path=False, rich_tracebacks=True)],
-    force=True,
-)
-logging.getLogger("httpx").setLevel(logging.WARNING)
-logging.getLogger("datasets").setLevel(logging.ERROR)
 
 __all__ = []
--- src/prep/constants.py
+++ src/prep/constants.py
@@ -1,5 +1,8 @@
+import logging
 import os
 import re
+
+from rich.logging import RichHandler
 
 
 def is_env_enabled(env_var: str, default: str = "0") -> bool:
@@ -29,3 +32,12 @@ SKIP = "SKIP"
 
 WARN_PREFIX = "⚠️\t"
 ERROR_PREFIX = "⛔\t"
+
+logging.basicConfig(
+    format="%(message)s",
+    level=os.getenv("LOG_LEVEL") or logging.INFO,
+    handlers=[RichHandler(show_time=False, show_path=False, rich_tracebacks=True)],
+    force=True,
+)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger("datasets").setLevel(logging.ERROR)
